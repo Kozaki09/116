f1d7987c0848186e64aafadd25782323cf1e12e4
Kozaki09 <godwin0613@yahoo.com>
2024-12-14 20:02:46 +0800

    feat: removing privately owned books now remove from both personal and main library

--- routes/remove/books.js
+++ routes/remove/books.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { removeFromLibrary, buildRemoveQuery } = require('../../utils/dbHelpers');
+const { buildRemoveQuery, buildSearchQuery } = require('../../utils/dbHelpers');
 
 router.delete('/from_library', async (req, res) => {
     const { book_id } = req.query;
     const user_id = req.session.user_id;
-    const filters = {
+    const remove = {
         book_id: book_id,
         user_id: user_id
     }
@@ -18,11 +18,19 @@ router.delete('/from_library', async (req, res) => {
         return res.status(400).json({Error: "Missing User ID"});
     }
 
-    try {
-        const result = await buildRemoveQuery("book_copies", filters);
-        // const result = await removeFromLibrary(book_id, user_id);
+    const result = await buildSearchQuery("books", ["availability"], {id: book_id});
+    const isPrivate = result.rows[0].availability === "PRIVATE" ? true : false;
 
-        if (result.rowCount > 0) {
+    try {
+        let removed = 0;
+        if (isPrivate) {
+            const result = await buildRemoveQuery("books", {id: book_id});
+            removed += result.rowCount;
+        }
+        const result = await buildRemoveQuery("book_copies", remove);
+        removed += result.rowCount;
+
+        if (removed > 0) {
             return res.status(200).json({ message: "Book successfully removed from library" });
         } else {
             return res.status(404).json({ message: "Book not found in the library" });
